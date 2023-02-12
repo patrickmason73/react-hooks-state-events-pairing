47f607af0030cbe38580becd2abd390c2353710c
Patrick Mason <patrickmason73@gmail.com>
2023-02-11 21:58:21 -0700

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,32 @@
+import React, {useState} from "react";
 import video from "../data/video.js";
+import Video from "./Video.js";
 
 function App() {
   console.log("Here's your data:", video);
+const numberOfUpvotes = parseInt(video.upvotes)
+const numberOfDownvotes = parseInt(video.downvotes)
+const [upvotes, setUpvotes] = useState(numberOfUpvotes)
+const [downvotes, setDownvotes] = useState(numberOfDownvotes)
+const [hideButton, setHideButton] = useState(false)
 
+function handleUpvotes() {
+  setUpvotes(parseInt(upvotes + 1))
+  console.log(upvotes)
+}
+
+function handleDownvotes() {
+  setDownvotes(parseInt(downvotes + 1))
+}
+
+function handleHideButton() {
+  setHideButton(!hideButton)
+}
+
+const videoComments = video.comments
   return (
     <div className="App">
-      <iframe
-        width="919"
-        height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        frameBorder="0"
-        allowFullScreen
-        title="Thinking in React"
-      />
+      <Video handleHideButton={handleHideButton} hideButton={hideButton} video={video} videoComments={videoComments} handleUpvotes={handleUpvotes} handleDownvotes={handleDownvotes} upvotes={upvotes} downvotes={downvotes} />
     </div>
   );
 }
